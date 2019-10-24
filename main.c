1fe63a7858901e45a6186ca9882db7facf40aa7a
whackteachers <33785383+whackteachers@users.noreply.github.com>
2019-10-24 08:05:40 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,8 +2,7 @@
 #include<stdlib.h>
 
 int  len(char str[]);
-char* encrypt(char key1[], char key2[]);
-char* decrypt(char key1[], char key2[]);
+int gcd(int a, int b);
 char* cw1(int flag, char key1[], char key2[]);
 
 char text[] = "message";			//plain text
@@ -17,91 +16,6 @@ void main() {
 
 int len(char str[]) {
 	return sizeof(str) / sizeof(char);
-}
-
-char* encrypt(char key1[], char key2[]) {
-	int text_size = sizeof(text) / sizeof(char);		//get the length of text, key1, key 2
-	int key1_size = sizeof(key1) / sizeof(char);
-	int key2_size = sizeof(key2) / sizeof(char) - 1;
-	char* cipher = (char*)calloc(text_size, sizeof(char));
-
-	//encrypt all characters 1 by 1 with for loop with respect to the text length
-	for (int i = 0; i < text_size - 1; i++) {
-		// # all char can be treated as int/numbers, direct arithematic will be performed directly
-		
-		char letter = text[i] - 96;		//get the letter at position i
-		printf("%i\n", letter);
-		int j = i % key1_size;
-		int k = i % key2_size;
-		char key1_letter = key1[j] - 96;
-		char key2_letter = key2[k] - 96;
-		int n = key1_letter + key2_letter;
-		printf("%i   %i\n", key1_letter, key2_letter);
-		printf("%i\n", n);
-		n = n - 4;
-		char cletter = letter - n;
-		//cletter = letter - key1_letter - key2_letter + 4;
-		//encrypt text-key1+2 mod 26
-
-		//cletter = cletter % 26;
-		//carry out mod operation (ensure result ranged from 1 to 26)
-		while (cletter < 1 || cletter > 26) {
-			if (cletter < 1) {
-				cletter = cletter + 26;
-			}
-			else if (cletter > 26) {
-				cletter = cletter - 26;
-			}
-		}
-		cipher[i] = cletter + 96;
-		printf("cipher letter: %c (%i)\n", cletter + 96, cletter);
-		j++;
-		k++;
-		//printf("%i\n", letter+1);
-	}
-	return cipher;
-}
-
-char* decrypt(char key1[], char key2[]) {
-	int text_size = sizeof(text) / sizeof(char);		//get the length of text, key1, key 2
-	int key1_size = sizeof(key1) / sizeof(char);
-	int key2_size = sizeof(key2) / sizeof(char) - 1;
-	char* t = (char*)calloc(text_size, sizeof(char));
-	printf("%i  %i\n", key1_size, key2_size);
-	
-	//encrypt all characters 1 by 1 with for loop with respect to the text length
-	for (int i = 0; i < text_size - 1; i++) {
-		// # all char can be treated as int/numbers, direct arithematic will be performed directly
-		
-		char cletter = text[i]-96;		//get the letter at position i
-		int j = i % key1_size;
-		int k = i % key2_size;
-		char key1_letter = key1[j]-96;
-		char key2_letter = key2[k] - 96;
-		int n = key1_letter + key2_letter;
-		n = n - 4;
-		//encrypt text+key1-2 mod 26
-		char letter = cletter + n;
-
-		//cletter = cletter % 26;
-		//carry out mod operation (ensure result ranged from 1 to 26)
-		while (letter < 1 || letter > 26) {
-			if (letter < 1) {
-				letter = letter + 26;
-			}
-			else if (letter > 26) {
-				letter = letter - 26;
-			}
-		}
-		//printf("%i  %i\n", cletter, key1_letter);
-		t[i] = letter + 96;
-		
-		printf("cipher letter: %c (%i)\n", letter + 96, letter);
-		j++;
-		k++;
-	}
-
-	return t;
 }
 
 char* cw1(int flag, char key1[], char key2[]) {
